ba8e6906d1cdc4b700cfd12e452d2e7a1e7fd58a
developershahzod <114401738+developershahzod@users.noreply.github.com>
2025-06-09 10:36:44 +0500

    Update Home.jsx

--- client/src/components/common/Home/Home.jsx
+++ client/src/components/common/Home/Home.jsx
@@ -11,6 +11,8 @@ import { HomeViews } from '../../../constants/Enums';
 import GridProjectsView from './GridProjectsView';
 import GroupedProjectsView from './GroupedProjectsView';
 
+import DashboardStats from './DashboardStats';
+
 
 
 import styles from './Home.module.scss';
@@ -33,7 +35,7 @@ const Home = React.memo(() => {
 
   return (
     <div className={styles.wrapper}>
-{/*       <DashboardStats /> */}
+      <DashboardStats />
 
 
       <View />
